2e7bbc767edaaec071121faa3c84151c02910a0b
root <root@srv-1c.tko22.local>
2021-09-01 06:54:00 +0700

    saving uncommitted changes in /etc prior to apt run

--- backup_pg_base_ro.sh
+++ backup_pg_base_ro.sh
@@ -1,7 +1,3 @@
 #/bin/bash
 export PGPASSWORD=secret1c
-/opt/pgpro/1c-13/bin/pg_dump -h 127.0.0.1 -U postgres --format custom -b --section pre-data --section data --section post-data --verbose --file /backup/ro/ro_backup$(date +%Y-%m-%d).bkp ro_backup
 /opt/pgpro/1c-13/bin/pg_dump -h 127.0.0.1 -U postgres --format custom -b --section pre-data --section data --section post-data --verbose --file /backup/ro/ro-$(date +%Y-%m-%d).bkp ro
-/opt/pgpro/1c-13/bin/pg_dump -h 127.0.0.1 -U postgres --format custom -b --section pre-data --section data --section post-data --verbose --file /backup/do/do-$(date +%Y-%m-%d).bkp do
-/opt/pgpro/1c-13/bin/pg_dump -h 127.0.0.1 -U postgres --format custom -b --section pre-data --section data --section post-data --verbose --file /backup/zup/zup-$(date +%Y-%m-%d).bkp zup
-/opt/pgpro/1c-13/bin/pg_dump -h 127.0.0.1 -U postgres --format custom -b --section pre-data --section data --section post-data --verbose --file /backup/bp/bp-$(date +%Y-%m-%d).bkp bp
