f68504647341a78890eeebd0d5ef3e69b186b504
root <root@srv-1c.tko22.local>
2021-09-28 13:59:16 +0700

    daily autocommit

--- backup_pg_bases.sh
+++ backup_pg_bases.sh
@@ -4,8 +4,8 @@ export PGPASSWORD=secret1c
 find /backup/ro/ -ctime +4 -name "*.bkp" -exec rm {} \;
 /opt/pgpro/1c-13/bin/pg_dump -h 127.0.0.1 -U postgres --format custom -b --section pre-data --section data --section post-data --verbose --file /backup/ro/ro-$(date +%Y-%m-%d).bkp ro
 /opt/pgpro/1c-13/bin/pg_dump -h 127.0.0.1 -U postgres --format custom -b --section pre-data --section data --section post-data --verbose --file /backup/do/do-$(date +%Y-%m-%d).bkp do
-find /backup/do/ -ctime +8 -name "*.bkp" -exec rm {} \
+find /backup/do/ -ctime +8 -name "*.bkp" -exec rm {} \;
 /opt/pgpro/1c-13/bin/pg_dump -h 127.0.0.1 -U postgres --format custom -b --section pre-data --section data --section post-data --verbose --file /backup/zup/zup-$(date +%Y-%m-%d).bkp zup
-find /backup/zup/ -ctime +8 -name "*.bkp" -exec rm {} \
+find /backup/zup/ -ctime +8 -name "*.bkp" -exec rm {} \;
 /opt/pgpro/1c-13/bin/pg_dump -h 127.0.0.1 -U postgres --format custom -b --section pre-data --section data --section post-data --verbose --file /backup/bp/bp-$(date +%Y-%m-%d).bkp bp
-find /backup/bp/ -ctime +8 -name "*.bkp" -exec rm {} \
+find /backup/bp/ -ctime +8 -name "*.bkp" -exec rm {} \;
